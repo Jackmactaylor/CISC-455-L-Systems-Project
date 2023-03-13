2733232f2c769a19574ab79bce3ae3c9aea70622
Jack Taylor <jackmactaylor@gmail.com>
2023-03-13 14:13:41 -0400

    Project Commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Class is meant to slowly rotate the camera around a rotate position
+public class CameraController : MonoBehaviour
+{
+    
+    public Transform rotatePosition;
+    //Speed of rotation
+    public float speed = 20f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotate the camera around the rotate position
+        transform.RotateAround(rotatePosition.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/SunMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script that handles the movement of the sun across the sky. Is attached to the directional light in the scene.
+public class SunMovement : MonoBehaviour
+{
+    
+    //Speed of rotation
+    public float speed = 20f;
+    //Transform to rotate around
+    public Transform rotatePosition;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotate the sun around the rotate position
+        transform.RotateAround(rotatePosition.position, Vector3.right, speed * Time.deltaTime);
+        //Make the directional lights rotation point at the rotate position
+        transform.LookAt(rotatePosition);
+    }
+}
